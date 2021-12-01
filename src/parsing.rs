e547fd207f523615a749f96f232296c2095a6940
Michael Diamond <dimo414@gmail.com>
2021-11-30 20:29:16 -0800

    Add tests for parsing mod

--- src/parsing.rs
+++ src/parsing.rs
@@ -18,3 +18,24 @@ pub fn capture_group<'a>(captures: &'a Captures, group: usize) -> &'a str {
     captures.get(group)
         .ok_or_else(|| format!("Invalid capture group {} for {:?}", group, captures)).unwrap().as_str()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn matches() {
+        let re = static_regex!("Foo.*(B.*r)");
+        let caps = regex_captures(re, "FooFarBaaar").unwrap();
+        assert_eq!(capture_group(&caps, 0), "FooFarBaaar");
+        assert_eq!(capture_group(&caps, 1), "Baaar");
+    }
+
+    #[test]
+    fn no_match() {
+        let re = static_regex!("Foo.*(B.*r)");
+        let caps = regex_captures(re, "Foo");
+        let err = caps.expect_err("Should not match").to_string();
+        assert!(err.contains("did not match"), "Was: {}", err);
+    }
+}
