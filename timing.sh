a0b3247afc59f263ac01058fb47c394117f74f84
Michael Diamond <dimo414@gmail.com>
2022-01-07 11:42:45 -0800

    Modify timing script to average three runs and track the accumulated runtime.

--- timing.sh
+++ timing.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 # Prints a Markdown table of the size and runtime of each solution.
 
+accum=()
+
+# Borrowed from bkt's benchmark script
+avg_floats() {
+  python <(cat <<EOF
+import sys
+total = sum((float(arg) for arg in sys.argv[1:]))
+print("{:.3f}".format(total/(len(sys.argv)-1)))
+EOF
+    ) "$@"
+}
+
+# Borrowed from bkt's benchmark script
+sum_floats() {
+  python <(cat <<EOF
+import sys
+total = sum((float(arg) for arg in sys.argv[1:]))
+print("{:.3f}".format(total))
+EOF
+    ) "$@"
+}
+
 # Borrowed from bash-cache's bc::_time
 runtime() {
   (
@@ -10,21 +32,29 @@ runtime() {
 }
 
 row() {
-  local binary="target/release/${1}" binary_size
+  local binary="target/release/${1}" binary_size runtimes=() avg_runtime
   binary_size=$(stat -c %s "$binary" 2>/dev/null) || return
+
+  for i in {0..3}; do
+    runtimes+=("$(runtime "$binary")")
+  done
+
+  # don't count the first run (:1 syntax) as it can be slower on some systems
+  avg_runtime=$(avg_floats "${runtimes[@]:1}")
+  accum+=("$avg_runtime")
 
   printf '| %12s ' \
     "${1#0}" \
     "$(( binary_size / 1024 ))KB" \
-    "$(runtime "$binary")s" \
-    "$(runtime "$binary")s"
+    "${avg_runtime}s" \
+    "$(sum_floats "${accum[@]}")s"
   echo '|'
 }
 
 cargo clean --release
 cargo build --release
 
-printf '| %12s ' "Day" "Binary Size" "Runtime" "Repeated"; echo '|'
+printf '| %12s ' "Day" "Binary Size" "Runtime" "Accumulated"; echo '|'
 echo '|--------------|--------------|--------------|--------------|'
 for i in {01..25}; do
   row "$i"
